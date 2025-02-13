7a03ca32ca5066d757bd4941c60c0bcaff3a7e34
simarjot <simarjot0032@gmail.com>
2025-02-13 18:12:35 +0530

    added some sample content to undefined page

---
+++ src/app/Generator/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <div
+        className="parent-container"
+        style={{
+          height: "100vh",
+          color: "white",
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "center",
+          fontFamily: "Space Grotesk",
+        }}
+      >
+        Password Generator Page under build
+      </div>
+    </>
+  );
+}

8b03b02a4d11b0a2207b764864f2ffff5447698c
simarjot <simarjot0032@gmail.com>
2025-02-13 18:04:39 +0530

    Home page completed

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 import "@Styles/Home.scss";
 import Logo from "@assets/Logo.webp";
+
 import { useState } from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { BsDatabaseLock } from "react-icons/bs";
+
 import FeatureCard from "./Components/FeatureCard";
 import FeatureCardData from "@Data/FeatureCard";
 import "@Styles/UniversalStyle.scss";
@@ -11,6 +14,7 @@ import FAQData from "@/Data/FAQData";
 import Footer from "@Compoenets/Footer";
 import Navbar from "@Compoenets/Navbar";
 import Product from "./Components/Product";
+import { ProductsArray } from "@/Data/Products";
 
 export default function HomePage() {
   const [questionIndex, setQuestionIndex] = useState<Number>(-1);
@@ -43,7 +47,22 @@ export default function HomePage() {
               </div>
             </div>
             <div className="home-content-container">
-              <div className="company-name">Everything You Need</div>
+              <div className="products-container">
+                <h1 className="products-heading">Our Products</h1>
+                <div className="products-content-container">
+                  {ProductsArray.map((ProductData) => {
+                    return (
+                      <Product
+                        ProductImage={ProductData.productIcon}
+                        productHeading={ProductData.productName}
+                        productDescription={ProductData.productDescription}
+                        productLink={ProductData.productLink}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="features-tagline">Everything You Need</div>
               <div className="features-container">
                 {FeatureCardData.map((cardData, index) => {
                   return (
@@ -55,10 +74,7 @@ export default function HomePage() {
                   );
                 })}
               </div>
-              <div className="products-container">
-                <h1 className="products-heading">Our Products</h1>
-                <Product />
-              </div>
+
               <div className="faq-parent-container">
                 <h2 className="faq-heading">Frequently Asked Questions</h2>
                 {FAQData.map((question, index) => {
